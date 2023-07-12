2053016c3fc833160ecbe0f7dc3b674d3e59dcc3
dongjun <sdj799@naver.com>
2023-07-12 18:19:06 +0900

    리엑트 네이티브 설정중

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>다른 텍스트 입니다!!!</Text>
+      </View>
+      <Text 
+        style={{
+          margin: 20, 
+          borderWidth: 2, 
+          borderColor: 'red', 
+          padding: 16
+          }}>
+            Hello World!!!
+      </Text>
+      <Button title='눌러보세요!'/>
     </View>
   );
 }
