a89498ae93218e07e9f76ba32e41dcebdedf73d4
dongjun <sdj799@naver.com>
2023-07-17 18:05:09 +0900

    # 7. 스타일 변경 todo 앱 완료

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -33,10 +33,14 @@ const GoalInput = (props) => {
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title='할 일 추가하기' onPress={addGoalHandler} />
+            <Button
+              title='할 일 추가하기'
+              onPress={addGoalHandler}
+              color='#b180f0'
+            />
           </View>
           <View style={styles.button}>
-            <Button title='취소' onPress={props.onCancle} />
+            <Button title='취소' onPress={props.onCancle} color='#f31282' />
           </View>
         </View>
       </View>
@@ -65,9 +69,12 @@ const styles = StyleSheet.create({
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "#cccccc",
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
+    color: "#120438",
+    borderRadius: 6,
     width: "100%",
-    padding: 8,
+    padding: 16,
   },
   buttonContainer: {
     marginTop: 16,
